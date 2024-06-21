614f0a5a7acdd86986f9b33d5ff380938002df38
Anugrah <pnanugrah@gmail.com>
2024-06-20 19:46:01 -0700

    fixed terminal startup on nvim start

--- after/plugin/fterm.lua
+++ after/plugin/fterm.lua
@@ -1,4 +1,4 @@
-require('FTerm').toggle()
+-- require('FTerm').toggle() If this is here, then it will toggle the terminal on startup
 
 -- or create a vim command
 vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
